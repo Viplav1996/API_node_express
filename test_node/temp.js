dffe297398e946d60a73735c1732fe738eb5de02
Viplav Nayyar <52647445+Viplav1996@users.noreply.github.com>
2021-01-16 00:36:10 -0500

    Add files via upload

---
+++ test_node/temp.js
@@ -0,0 +1,17 @@
+
+var rs = require('readline-sync');
+var v1 = 10;
+var v2 = 5;
+var sum = v1+v2;
+var lag = "done";
+
+console.log(sum);
+console.log(lag);
+
+var name = rs.question("What is your name?");
+
+console.log("Your name is ", name);
+
+
+
+
